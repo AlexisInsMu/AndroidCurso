4ff6b010de713b226be6b8160cf72269ead23c3e
ALEXIS ADRIAN MURILLO BARRIENTOS <amurillob2000@alumno.ipn.mx>
2023-01-26 16:53:51 -0600

    android first app finish

--- app/src/main/java/com/alexisnovo/androidcurso/firstapp/FirstAppActivity.kt
+++ app/src/main/java/com/alexisnovo/androidcurso/firstapp/FirstAppActivity.kt
@@ -1,7 +1,12 @@
 package com.alexisnovo.androidcurso.firstapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.alexisnovo.androidcurso.R
 
 class FirstAppActivity : AppCompatActivity() {
@@ -9,5 +14,20 @@ class FirstAppActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_first_app)
 		//Al arrancar la pantalla
+		val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+		val editText = findViewById<AppCompatEditText>(R.id.etName)
+
+		btnStart.setOnClickListener {
+			val name = editText.text.toString()
+			if(name.isNotEmpty()){
+				val intent = Intent(this, ResultActivity::class.java)
+				intent.putExtra("EXTRA_NAME", name)
+				startActivity(intent)
+				Log.i("Alexis", "Button pulsado ${editText.text.toString()}")
+			}else{
+				Log.i("Alexis","ESCRIBE FLOJO")
+			}
+		}
+
 	}
 }
